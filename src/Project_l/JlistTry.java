6babcb9ac8006083d7807d4ff49f3730ef252347
AzureD <a1714656568@gmail.com>
2021-04-21 09:03:40 +0800

    练习了JList对象的使用，现在会初始化和刷新JList中的内容了^_^

--- src/Project_l/JlistTry.java
+++ src/Project_l/JlistTry.java
@@ -1,31 +1,43 @@
 package Project_l;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.util.Vector;
 
 public class JlistTry extends JFrame{
+    Vector<String> v =new Vector<String>();
     public JlistTry(){
         init();
     }
     public void init(){
       JList jlist;
-      Vector<String> v =new Vector<String>();
       jp=new JPanel();
       jf=new JFrame();
-      jl=new JList(v);
+      jb=new JButton("init");
+      jb.setBounds(10,10,10,10);
+      v.add("hello");
+      v.add("world");//idea中可以选中代码然后按tab向右缩进，也可以选中之后按shift+tab实现向左缩进。
+      jl=new JList(v);//初始化的时候可以用这个构造方法构建list，初始化list内容
       jf.add(jp);
       jf.setSize(100,100);
       jp.add(jl);
+      jp.add(jb);
       jf.setVisible(true);
-      v.add("hello");
-      v.add("world");
-      jl.setListData(v);
-    }
+      jb.addActionListener(e -> init(e));
 
+//      jl.setListData(v);//可以加上按钮监听，实现刷新list内容的方法。
+    }
+    public void init(ActionEvent e){
+        v.clear();
+        v.add("first");
+        v.add("second");
+        jl.setListData(v);
+    }
     public static void main(String[] args) {
         JlistTry j=new JlistTry();
     }
     public JPanel jp;
     public JFrame jf;
     public JList jl;
+    public JButton jb;
 }
